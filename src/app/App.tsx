4cc3c63b416ad455ed06fc8940f5bc584a13c588
Tharmes <tharmeselvam@gmail.com>
2025-12-16 13:57:17 +0800

    Implement axios and set up navigation

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,28 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { useColorScheme } from 'react-native';
 import { RootNavigator } from './RootNavigator';
+import { Provider, useDispatch } from 'react-redux';
+import { AppDispatch, store } from '../store';
+import { useEffect } from 'react';
+import { checkAuth } from '../store/slices/authSlice';
+
+function AppBootstrap() {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+        dispatch(checkAuth());
+    }, [dispatch]);
+
+  return <RootNavigator />
+}
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <AuthProvider>
+    <Provider store={store}>
       <NavigationContainer>
-        <RootNavigator />
+        <AppBootstrap />
       </NavigationContainer>
-    </AuthProvider>
+    </Provider>
   );
 }
 
