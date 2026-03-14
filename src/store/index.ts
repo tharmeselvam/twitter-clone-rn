8244d9fe35c4ca8790dd9427c63d9fbc911050d6
tharmeselvam <tharmeselvam@gmail.com>
2026-03-14 19:36:31 +0800

    Set up profile slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit"
 import authReducer from './slices/authSlice'
 import homeReducer from "./slices/homeSlice"
 import searchReducer from "./slices/searchSlice"
+import profileReducer from "./slices/profileSlice"
 
 export const store = configureStore({
     reducer: { 
         auth: authReducer,
         home: homeReducer,
         search: searchReducer,
+        profile: profileReducer,
     }
 });
 
